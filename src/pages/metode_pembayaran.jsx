c92fcee7f1d0139158453c4fe34c8eef5f9179c0
suryaharis22 <suryaharis22@gmail.com>
2024-02-07 10:27:04 +0700

    update

--- src/pages/metode_pembayaran.jsx
+++ src/pages/metode_pembayaran.jsx
@@ -123,7 +123,7 @@ const MetodePembayaran = () => {
                             </div>
                             <input id="default-radio-1" type="radio" value="ID_LINKAJA" name="default-radio" onChange={handleRadioChange} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
                         </label>
-                        <label for="default-radio-2" className="my-2 flex items-center justify-between border-gray-500 bottom-1 border rounded-lg h-14 px-4">
+                        {/* <label for="default-radio-2" className="my-2 flex items-center justify-between border-gray-500 bottom-1 border rounded-lg h-14 px-4">
                             <div className="flex items-center">
                                 <img src="icon/payment/gopay.png" alt="" className="w-10 h-10 rounded-lg" />
                                 <p className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">
@@ -131,7 +131,7 @@ const MetodePembayaran = () => {
                                 </p>
                             </div>
                             <input id="default-radio-2" type="radio" value="Gopay" name="default-radio" onChange={handleRadioChange} className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
-                        </label>
+                        </label> */}
 
                     </div>
 
