6b6be44c7f7abf77ad892c91a055b20abb98055a
Aufaarib <aufa@nuncorp.id>
2024-03-18 14:17:04 +0700

    cannot build

--- src/components/page/FormMerchant.jsx
+++ src/components/page/FormMerchant.jsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import React, { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
 import { StepOne, StepTwo } from "../FormCampaing/StepMerchant";
 import Header from "../Header";
 import Loading from "../Loading";
