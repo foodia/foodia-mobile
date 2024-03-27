2f4dc9bd1a391344dc5342b44e35a60cfe13b8a1
suryaharis22 <suryaharis22@gmail.com>
2024-03-27 14:56:11 +0700

    validasi image

--- src/components/FormCampaing/StepMerchant.jsx
+++ src/components/FormCampaing/StepMerchant.jsx
@@ -98,7 +98,7 @@ function StepOne({ registrasiMerchant, setRegistrasiMerchant }) {
   const handlektp_photoChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      const allowedTypes = ["image/png", "image/jpeg", "image/jpg"];
+      const allowedTypes = ["image/png", "image/jpeg", "image/jpg", "image/heif", "image/heic"];
       const maxSize = 5 * 1024 * 1024; // 5MB
 
       if (!allowedTypes.includes(file.type)) {
