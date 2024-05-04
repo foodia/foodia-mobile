a0b6580da7902a819dc023df251d18f4e9a8ca2d
Aufaarib <aufa@nuncorp.id>
2024-05-04 15:53:05 +0700

    re-slicing mydonation,bukti pembayaran,detail campaign ui/ux, integrating with api

--- src/pages/campaign/[id].jsx
+++ src/pages/campaign/[id].jsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import Loading from "@/components/Loading";
 import Error401 from "@/components/error401";
 import DetailCamp from "@/components/page/DetailPage";
 import axios from "axios";
@@ -8,6 +9,7 @@ import { useEffect, useState } from "react";
 const Campaign = () => {
   const router = useRouter();
   const { id } = router.query;
+  const [loading, setLoading] = useState(true);
   const [campaignData, setCampaignData] = useState(null);
 
   useEffect(() => {
@@ -23,9 +25,10 @@ const Campaign = () => {
           }
         );
         setCampaignData(response.data.body);
+        setLoading(false);
       } catch (error) {
+        setLoading(false);
         if (error.response && error.response.status === 401) {
-
           Error401(error, router);
         }
       }
@@ -38,9 +41,10 @@ const Campaign = () => {
 
   return (
     <div className="h-full max-w-480 bg-white flex flex-col">
-      <Header title="Informasi" backto="/home" />
+      <Header title="Informasi" />
       {campaignData && <DetailCamp data={campaignData} />}
       {/* <BottomNav /> */}
+      {loading && <Loading />}
     </div>
   );
 };
