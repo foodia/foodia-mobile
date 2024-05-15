de936dbbc989f71c2f769a1a6dc7b687eeed16cf
suryaharis22 <suryaharis22@gmail.com>
2024-05-15 16:03:56 +0700

    perbaikan upload reting as detonator

--- src/components/CompressImage.js
+++ src/components/CompressImage.js
@@ -5,7 +5,7 @@ const CompressImage = (file) => {
     return new Promise((resolve, reject) => {
         new Compressor(file, {
             quality: 1,
-            maxWidth: 1024,
+            // maxWidth: 1024,
             success(result) {
                 resolve(result);
             },
